644694423dcc3a15f5bc4619f6ba6a5f68e38882
JOHANA403 <johaliza.23@gmail.com>
2024-01-29 13:34:05 -0800

    week1 solved

---
+++ src/Johana/week1/oddOrEven.java
@@ -0,0 +1,12 @@
+package Johana.week1;
+
+public class oddOrEven {
+    public static void main(String[] args) {
+
+    }
+    /*Write  a method which can identify given number is even or odd
+    Output ex:
+    identify(5); ->"Odd"
+    identify(6); ->"Even"*/
+
+}
