436ddeb24d09c304d01f5ef0e3449d496174a44b
LeilaSafavi <146497718+LeilaSafavi@users.noreply.github.com>
2024-01-31 08:29:08 -0800

    Update finra.java

--- src/LeilaS/week1/finra.java
+++ src/LeilaS/week1/finra.java
@@ -1,6 +1,22 @@
 package LeilaS.week1;
 
 public class finra {
+    public static void main(String[] args) {
+        printNumbers();
+    }
+    public static void printNumbers() {
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+        }
     //Write a method which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3,
     //print "FIN" instead of the number and
     //for numbers which are a multiple of 5, print "RA" instead of the number.
