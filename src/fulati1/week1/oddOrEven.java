457321275a884a696f9832ab2fc924a70d621aa8
fulatiaisaiti <asatpulat@gmail.com>
2024-01-31 14:44:17 -0800

    week1 questions answered

--- src/fulati1/week1/oddOrEven.java
+++ src/fulati1/week1/oddOrEven.java
@@ -1,10 +1,32 @@
 package fulati1.week1;
 
 public class oddOrEven {
-    /*Write  a method which can identify given number is even or odd
-    Output ex:
-    identify(5); ->"Odd"
-    identify(6); ->"Even" kkkk*/
+    public static String identifyNumber(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
 
+    public static void main(String[] args) {
+
+        String a = identifyNumber(2);
+        String b = identifyNumber(6);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
 
 }
+
+
+/*
+
+Numbers - Odd or even:
+Write  a method which can identify given number is even or odd
+Output ex:
+identify(5); ->"Odd"
+identify(6); ->"Even"
+
+ */
