3eb6cead597d2bed49102eb393ec4208a053d5bf
JOHANA403 <johaliza.23@gmail.com>
2024-01-30 16:02:50 -0800

    week1 task solved

--- src/Johana/week1/divisionWithoutOperator.java
+++ src/Johana/week1/divisionWithoutOperator.java
@@ -1,6 +1,27 @@
 package Johana.week1;
 
 public class divisionWithoutOperator {
-    //Write a method that can divide two numbers without using division operator
+    public static void main(String[] args) {
+
+
+        int a = 27;
+        int b = 9;
+
+        int count = 0;
+
+        while (a >= b) {
+            a -= b;
+            count++;
+        }
+
+        System.out.println(count+" with a remainder of "+a);
+
+
+    }
+
 
 }
+
+//Write a method that can divide two numbers without using division operator
+
+
--- src/Johana/week1/oddOrEven.java
+++ src/Johana/week1/oddOrEven.java
@@ -1,12 +1,25 @@
 package Johana.week1;
 
+import java.util.Scanner;
+
 public class oddOrEven {
     public static void main(String[] args) {
 
+        int number= 6;
+
+        boolean isEven =  number % 2 == 0;
+        boolean isOdd = !isEven;
+        System.out.println(number + " is an even number: " + isEven);
+        System.out.println(number + " is an odd number: " + isOdd);
+
+
     }
-    /*Write  a method which can identify given number is even or odd
+
+}
+
+   /*Write  a method which can identify given number is even or odd
     Output ex:
     identify(5); ->"Odd"
     identify(6); ->"Even"*/
 
-}
+
