9a0565b34643d4c63bca5886eaa1962b12c7a543
Fulati Aizihaer <azhar.pulat@gmail.com>
2024-02-03 00:15:36 -0800

    Solved Week2 Task

--- src/Fulati2/week2/divisibleBy3_5_15.java
+++ src/Fulati2/week2/divisibleBy3_5_15.java
@@ -12,6 +12,36 @@ Divisible By 5 5 10 20 25 35 40 50 55 65 70 80 85 95 100
 Divisible By 3 3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72 78 81 84 87 93 96 99
 */
 
+    public static void main(String[] args) {
+
+        System.out.println("OutPut:");
+
+        System.out.print("Divisible By 15 ");
+        for (int i = 1; i <= 100; i++) {
+            if((i % 3) == 0 && (i % 5) == 0 && (i % 15) == 0){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.print("Divisible By 5 ");
+        for (int i = 1; i <= 100; i++) {
+            if(i % 5 == 0){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.print("Divisible By 3 ");
+        for (int i = 1; i <= 100; i++) {
+            if(i % 3 == 0){
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+
 
 
 }
--- src/Fulati2/week2/printConsecutiveNumbers.java
+++ src/Fulati2/week2/printConsecutiveNumbers.java
@@ -17,7 +17,7 @@ public class printConsecutiveNumbers {
         7
         Codility
         Test
-        Codi1ityCoders
+        CodilityCoders
         11
         CodilityTest
         13
@@ -26,5 +26,32 @@ public class printConsecutiveNumbers {
         Codility
         17
 */
+
+    public static void main(String[] args) {
+        printConsecutiveNum(17);
+    }
+
+    public static void printConsecutiveNum(int N){
+        for (int i = 1; i <= N; i++) {
+            if ((i % 2 == 0) && (i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("CodilityTestCoders");
+            } else if ((i % 2 == 0) && (i % 3 == 0)){
+                System.out.println("CodilityTest");
+            } else if ((i % 2 == 0) && (i % 5 == 0)){
+                System.out.println("CodilityCoders");
+            } else if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("TestCoders");
+            }else if ((i % 2 == 0)){
+                System.out.println("Codility");
+            }else if ((i % 3 == 0)){
+                System.out.println("Test");
+            }else if ((i % 5 == 0)){
+                System.out.println("Coders");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
 
 }
