ae0dadd18cb60d131333ff2dcd3281966f7b04ca
JOHANA403 <johaliza.23@gmail.com>
2024-02-07 06:17:53 -0800

    week2 tasks done

--- src/Johana/week2/divisibleBy3_5_15.java
+++ src/Johana/week2/divisibleBy3_5_15.java
@@ -1,10 +1,31 @@
 package Johana.week2;
 public class divisibleBy3_5_15 {
+    public static void main(String[] args) {
+        int n = 100;
+        String by15 = "", by5 = "", by3 = "";
+
+        for(int i = 1; i <= n; i++){
+
+            if(i % 15 == 0){
+                by15 += i + " ";
+            } else if(i % 5 == 0){
+                by5 += i + " ";
+            } else if (i % 3 == 0){
+                by3 += i + " ";
+            }
+        }
+
+        System.out.println("Divisible by 15: " + by15);
+        System.out.println("Divisible by 5: " + by5);
+        System.out.println("Divisible by 3: " + by3);
+
+    }
+}
 /*Numbers -- Divisible by 3, 5, 15
 Write a program that can print the numbers between 1 ~ 100 that can be divisible by 3, 5, and 15.
-if the number can be divisible by 3, 5 and 15, then it should only be displayed in DivisibelBy15' section
-if the number can be divisible by 3 but cannot be divisible by 15, then it should only be displayed in DivisibelBy3' section
-if the number can be divisible by 5 but cannot be divisible by 15, then it should only be displayed in DivisibelBy5' section
+if the number can be divisible by 3, 5 and 15, then it should only be displayed in DivisibleBy15' section
+if the number can be divisible by 3 but cannot be divisible by 15, then it should only be displayed in DivisibleBy3' section
+if the number can be divisible by 5 but cannot be divisible by 15, then it should only be displayed in DivisibleBy5' section
 ex:
 OutPut:
 Divisible By 15 15 30 45 60 75 90
@@ -14,5 +35,5 @@ Divisible By 3 3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72 78 81 84
 
 
 
-}
+
 
--- src/Johana/week2/printConsecutiveNumbers.java
+++ src/Johana/week2/printConsecutiveNumbers.java
@@ -1,5 +1,31 @@
 package Johana.week2;
 public class printConsecutiveNumbers {
+    public static void main(String[] args) {
+        consecutiveNumbers(24);
+    }
+    public static void consecutiveNumbers(int n){
+
+        for(int i = 1; i <= n; i++){
+            String result = "";
+
+            if(i % 2 == 0){
+                result += "Codility";
+            }
+
+            if(i % 3 == 0){
+                result += "Test";
+            }
+
+            if(i % 5 == 0){
+                result += "Coders";
+            }
+
+            System.out.println(result.isEmpty() ? i : result);
+        }
+
+    }
+
+
          /*  Write a function: that, given a positive integer N, prints the consecutive numbers from 1 to N,
       each on a separate line. However, any number divisible by 2, 3 or 5 should be replaced by the word Codility,
       Test or Coders respectively. If a number is divisible by more than one of the numbers: 2,3 or 5,
