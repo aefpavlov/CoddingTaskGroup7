83e716ecefe43abe565c7cf38f4e3b878d77420a
fulatiaisaiti <asatpulat@gmail.com>
2024-02-07 12:29:57 -0800

    week2 solution

--- src/fulati1/week2/swapTwoNumbers.java
+++ src/fulati1/week2/swapTwoNumbers.java
@@ -1,7 +1,19 @@
 package fulati1.week2;
+
 public class swapTwoNumbers {
     //Swap two variable' values without using a third variable
     public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
 
     }
 }
