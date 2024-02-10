ccc65cf148ef5cced691220bb658206a17685e3a
Fulati Aizihaer <azhar.pulat@gmail.com>
2024-02-09 22:51:03 -0800

    Solved Week3 Task

--- src/Fulati2/week3/reverseNegativeNumber.java
+++ src/Fulati2/week3/reverseNegativeNumber.java
@@ -1,7 +1,5 @@
 package Fulati2.week3;
 
-import java.sql.SQLOutput;
-
 public class reverseNegativeNumber {
     //Write a return method that can reverse digits of a negative number and return it as int
     //input: -35  output: -53
