475cad7c25f36268c86bb2c4a998aef0355985c3
aefpavlov <aefpavlov@gmail.com>
2024-02-10 22:52:34 -0800

    week 3 Reversed Number solution

---
+++ src/angel/week3/primeNumber.java
@@ -0,0 +1,34 @@
+package angel.week3;
+
+import java.util.Scanner;
+
+public class primeNumber {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter whole  number");
+        int entry = input.nextInt();
+
+        System.out.println("Number you have enter "+entry+" is "+(isPrime(entry)? " Prime":" Not Prime" + " number"));
+
+        input.close();
+
+    }
+
+     // Write a method that can check if a number is prime or not
+   public static boolean isPrime(int number)
+    {
+        for (int i = 2; i < number; i++)
+            // loop to divide entered numbers to all numbers until it
+        {
+            if((number% i) == 0) // finds other number that can divide entry => no prime
+            {
+                // Not Prime
+                return false; // no need to continue if can be divided by other
+            }
+        }
+
+        return true;//  Prime
+    }
+
+}
--- src/angel/week3/reverseNegativeNumber.java
+++ src/angel/week3/reverseNegativeNumber.java
@@ -3,8 +3,8 @@ package angel.week3;
 import java.util.Scanner;
 
 public class reverseNegativeNumber {
-    //Write a return method that can reverse digits of a negative number and return it as int
-    //input: -35  output: -53
+    /*Write a return method that can reverse digits of a negative number and return it as int
+    input: -35  output: -53 */
 
     public static void main(String[] args)
     {
