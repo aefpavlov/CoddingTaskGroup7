bc63c0032b18f03ba72cd3c51385010de41b2699
JOHANA403 <johaliza.23@gmail.com>
2024-02-11 05:26:59 -0800

    week3 finished tasks

--- src/Johana/week3/primeNumber.java
+++ src/Johana/week3/primeNumber.java
@@ -1,9 +1,39 @@
 package Johana.week3;
 
+import java.util.Scanner;
+
 public class primeNumber {
      // Write a method that can check if a number is prime or not
 
 
+// prime number is a number that is greater than 1 and divided by 1 or itself only.
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number to check if it is prime or not: ");
+        int number= sc.nextInt();
+        if(isPrime(number)) {
+            System.out.println(number + " is prime number");
+        }
+        else{
+            System.out.println(number + " is a non-prime number");
+        }
+    }
 
+    public static boolean isPrime(int n){
+
+        if(n <= 1){
+            return false;
+        }
+
+        for(int i = 2; i < n; i++){
+
+            if(n % i == 0){
+                return false;
+            }
+
+        }
+
+        return true;
+    }
 
 }
