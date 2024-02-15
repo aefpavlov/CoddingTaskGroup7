2f8d01692eee3c132519084489388584dea7dee8
fulati <faizihaer@gmail.com>
2024-02-14 17:09:49 -0800

    Updated Week3 Task

--- src/Fulati2/week3/reverseNegativeNumber.java
+++ src/Fulati2/week3/reverseNegativeNumber.java
@@ -4,7 +4,7 @@ public class reverseNegativeNumber {
     //Write a return method that can reverse digits of a negative number and return it as int
     //input: -35  output: -53
     public static void main(String[] args) {
-        reverseDigit(929);
+        reverseDigit(-500);
     }
 
     public static void reverseDigit(int num){
@@ -18,7 +18,7 @@ public class reverseNegativeNumber {
         for (int i = numStr.length()-1; i >= 1; i--) {
             newNum += numStr.charAt(i);
         }
-        System.out.println(Integer.parseInt(newNum));
+        System.out.println(newNum);
     }
 
 
