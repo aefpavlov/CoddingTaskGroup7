e2a6f70c08bb4087e4cc0460d274b05d524da137
aefpavlov <aefpavlov@gmail.com>
2024-02-20 00:04:57 -0800

    Week4 solutions

--- src/angel/week4/FrequencyofCharacters.java
+++ src/angel/week4/FrequencyofCharacters.java
@@ -2,7 +2,8 @@ package angel.week4;
 
 public class FrequencyofCharacters {
     /*Write a return method that can find the frequency of characters
-    Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2*/
+    Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+    */
     public static void main(String[] args)
     {
         String  s1= "AAABBCDD",
--- src/angel/week4/SameLetters.java
+++ src/angel/week4/SameLetters.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class SameLetters {
     /*
     Write a return method that check if a string is build out of the same letters as another string.
-    Ex:  same("abc",  "cab"); -> true
-    same("abc",  "abb"); -> false:
+    Ex:    same("abc",  "cab"); -> true
+          same("abc",  "abb"); -> false:
     */
     public static void main(String[] args)
     {
