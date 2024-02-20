d276a8ed11fd1ff151eb28e2aba0e4c5c2671d26
aefpavlov <aefpavlov@gmail.com>
2024-02-19 23:32:11 -0800

    Week 4 solutions

---
+++ src/angel/week4/SameLetters.java
@@ -0,0 +1,28 @@
+package angel.week4;
+
+import java.util.Arrays;
+
+public class SameLetters {
+    /*Write a return method that check if a string is build out of the same letters as another string.
+    Ex:  same("abc",  "cab"); -> true
+    same("abc",  "abb"); -> false:*/
+    public static void main(String[] args)
+    {
+        String s1 = "abc",
+                s2 = "cab" ,
+                s3 = "abb";
+        System.out.println(same(s1,s2));
+        System.out.println(same(s1,s3));
+        System.out.println(same(s2,s1));
+
+
+    }
+    public static boolean same( String str1 , String str2 ){
+        char[] str1Array = str1.toCharArray();
+        char [] str2Array = str2.toCharArray();
+        Arrays.sort(str1Array);
+        Arrays.sort(str2Array);
+        return Arrays.equals(str1Array,str2Array );
+    }
+
+}
