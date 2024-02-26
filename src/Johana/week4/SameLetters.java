283b68e5037ef17766cdd1479e1fa504df5342d3
JOHANA403 <johaliza.23@gmail.com>
2024-02-26 06:00:04 -0800

    Task are done for week 4 and 5

--- src/Johana/week4/SameLetters.java
+++ src/Johana/week4/SameLetters.java
@@ -1,8 +1,31 @@
 package Johana.week4;
 
+import java.util.Arrays;
+
 public class SameLetters {
     /*Write a return method that check if a string is build out of the same letters as another string.
     Ex:  same("abc",  "cab"); -> true
     same("abc",  "abb"); -> false:*/
+
+    public static void main(String[] args) {
+
+        String str1 = "abc"; //"aehtr"
+        String str2 = "abb"; //"aehtr"
+
+
+        // String[] a1 = str1.split("");
+
+        char[] a1 = str1.toCharArray();
+        char[] a2 = str2.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+
+        boolean isAnagram = Arrays.equals(a1, a2);
+
+        System.out.println("isAnagram = " + isAnagram);
+
+
+    }
 
 }
--- src/Johana/week5/reverse.java
+++ src/Johana/week5/reverse.java
@@ -4,5 +4,26 @@ public class reverse {
    /* Write a return method that can reverse  String
     Ex: Reverse("ABCD"); ==> DCBA*/
 
+    public static void main(String[] args) {
 
-}
+        //The reverse() method of the StringBuilder class will allow us
+        //to reverse the character sequence represented by the String.
+
+
+        String name = "ABCD"; // I am declaring the String
+
+        StringBuilder stringBuilder = new StringBuilder(name);//I created a new instance of the StringBuilder class as stringBuilder
+        //using "name" string (which I have to reverse).
+
+        String reversedString = stringBuilder.reverse().toString();//applied reverse() method on the stringBuilder instance
+        //and assigned to the same stringBuilder instance (to get the reversed String).
+
+        System.out.println(reversedString); //printed out the reversed String.
+    }
+
+
+
+
+    }
+
+
