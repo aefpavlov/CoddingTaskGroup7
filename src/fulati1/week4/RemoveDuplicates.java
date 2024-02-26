f22a72b5fa8b3ddf6d10f76db11306d68bbf83db
Cydeo-Fulati <asatpulat@gmail.com>
2024-02-26 14:39:30 -0800

    week3 solution update

---
+++ src/fulati1/week4/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+package fulati1.week4;
+
+public class RemoveDuplicates {
+    /*Write a return method that can remove the duplicated values from String
+        Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicate("abbbbcccdddd"));
+    }
+
+
+    public static String removeDuplicate(String word) {
+        String result = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            if (!result.contains("" + word.charAt(i)))
+
+                result += "" + word.charAt(i);
+        }
+
+
+        return result;
+    }
+
+}
