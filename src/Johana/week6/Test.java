51185bd3226d183c04f4aafe6953dc708439bc74
JOHANA403 <johaliza.23@gmail.com>
2024-03-05 04:41:46 -0800

    Completed tasks for week6

---
+++ src/Johana/week6/Test.java
@@ -0,0 +1,10 @@
+package Johana.week6;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        PasswordValidation passwordValidation = new PasswordValidation("Johana", "Cydeo123");
+        System.out.println(passwordValidation);
+    }
+}
