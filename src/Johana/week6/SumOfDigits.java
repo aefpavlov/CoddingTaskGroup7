9271b556a6550fbc4414adf546825c8f4c3b2722
JOHANA403 <johaliza.23@gmail.com>
2024-03-11 04:51:18 -0700

    Completed tasks for week6

--- src/Johana/week6/SumOfDigits.java
+++ src/Johana/week6/SumOfDigits.java
@@ -8,12 +8,12 @@ public class SumOfDigits {
 
         String str = "12 java 5 apple 3";
         //str=str.replaceAll("[\\D]+"," ");//replace the non-numbers from the string with space.
-        str = str.replaceAll("[\\D]]", " "); //   -->[//D] is a predefined that matches the non-digits
+        str = str.replaceAll("[\\D]]", " "); //   -->[//D] is a predefined character that matches the non-digits
         String[] numbers=str.split(" ");//split them
         int sum = 0;
         for(int i=0;i<numbers.length;i++){//use a for loop
             try{
-                sum+=Integer.parseInt(numbers[i]);
+                sum+=Integer.parseInt(numbers[i]);//returns the string as an integer number
             }
             catch( Exception e ) {
                 //if the element in the array is not parse-able into Integer, Ignore it
