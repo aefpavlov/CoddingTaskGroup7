1e2e068a8a171d26279be6e75baf2fe5ab4f2af0
neginarahimi <negina_rahimi@hotmail.com>
2024-03-18 14:56:30 -0700

    first commit

---
+++ src/Negina/week2/swapTwoNumbers.java
@@ -0,0 +1,5 @@
+package Negina.week2;
+public class swapTwoNumbers {
+    //Swap two variable' values without using a third variable
+
+}
---
+++ src/Negina/week3/primeNumber.java
@@ -0,0 +1,9 @@
+package Negina.week3;
+
+public class primeNumber {
+     // Write a method that can check if a number is prime or not
+
+
+
+
+}
